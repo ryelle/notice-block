35d9d5ec06dab129970590a81ff0ee4137aac4ec
Kelly Dwan <ryelle@users.noreply.github.com>
2022-06-16 16:24:02 -0400

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,9 @@
+=== Notice Block ===
+Contributors:      ryelle
+Tags:              block
+Tested up to:      6.0
+Stable tag:        0.1.0
+License:           GPL-2.0-or-later
+License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+
+Add a color-coded notice to your post.
---
+++ src/edit.js
@@ -0,0 +1,24 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { useBlockProps } from '@wordpress/block-editor';
+
+/**
+ * Internal dependencies
+ */
+import './editor.scss';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @return {WPElement} Element to render.
+ */
+export default function Edit() {
+	return (
+		<p { ...useBlockProps() }>
+			{ __( 'Notice Block – hello from the editor!', 'notice-block' ) }
+		</p>
+	);
+}
